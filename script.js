d1ec00a1831389cfb6d864f740e94f91a2a09d65
Rudy-0512 <rgonzalez-s@cps.edu>
2025-03-13 14:33:26 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+console.log(work)
+
+let age= document.querySelector(".age");
+let spider= document.querySelector(".spider");
+let button= document.querySelector(".button");
+
+button.onClick = funcation();{
+
+console.log("work button")
+let answerAge = Number(age.value);
+console.log(answerAge)
+let answerDog = Number (dog.value);
+console.log(answerDog)
+
+if(answerage === 12){
+    console.log("yep 12")
+}else if(age === 12){
+    console.log("21 yay")
+}
+
+};
