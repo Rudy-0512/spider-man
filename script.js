0616223bf1cb393fd1f131006717deb796559064
Rudy-0512 <rgonzalez-s@cps.edu>
2025-04-14 03:50:05 +0000

    v3

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
 console.log(work)
 
-let age= document.querySelector(".age");
-let spider= document.querySelector(".spider");
+let age= document.querySelector(".job");
+let spider= document.querySelector(".suit");
 let button= document.querySelector(".button");
+let normalSpider= document.getElementById("Normal-spider");
+let spiderSuit= document.getElementById("Spider-suit");
 
 button.onClick = funcation();{
 
-console.log("work button")
-let answerAge = Number(age.value);
-console.log(answerAge)
-let answerDog = Number (dog.value);
-console.log(answerDog)
+let job = Q1.value;
+console.log(job);
+let suit = Q2.value;
+console.log(suit);
 
-if(answerage === 12){
-    console.log("yep 12")
-}else if(age === 12){
-    console.log("21 yay")
+if(job === "Photographer" && suit === "One") {
+    console.log("Tobey")
+    result.textContent = "You are Tobey!"
+    normalSpider.style.display = "block";
+    spiderSuit.style.display = "block";
+} else if(job === "Engineer" && suit === "Two") {
+    result.textContent = "You are Tom!"
+} else if(job === "Genuis" && suit === "Three"){
+    result.textContent = "You are Andrew!"
 }
+    
+
+
 
 };
