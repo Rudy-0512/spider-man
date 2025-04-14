394633267f2002d461849151e2e29bb9a79692c2
Rudy-0512 <rgonzalez-s@cps.edu>
2025-04-14 14:39:08 +0000

    v4

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-console.log(work)
+console.log("check js");
 
-let age= document.querySelector(".job");
-let spider= document.querySelector(".suit");
-let button= document.querySelector(".button");
+let Q1= document.querySelector(".job");
+let Q2= document.querySelector(".suit");
+let button= document.querySelector("button");
 let normalSpider= document.getElementById("Normal-spider");
 let spiderSuit= document.getElementById("Spider-suit");
+let result=document.querySelector(".result");
+let h1 = document.querySelector("h1");
 
-button.onClick = funcation();{
 
-let job = Q1.value;
+button.onclick = function() {
+    console.log("button check");
+    let job = Q1.value;
 console.log(job);
 let suit = Q2.value;
 console.log(suit);
 
 if(job === "Photographer" && suit === "One") {
-    console.log("Tobey")
-    result.textContent = "You are Tobey!"
-    normalSpider.style.display = "block";
-    spiderSuit.style.display = "block";
+console.log("Tobey")
+h1.innerHTML = "You are Tobey!";
+normalSpider.src = "pick.jpg";
+spiderSuit.style.display = "block";
 } else if(job === "Engineer" && suit === "Two") {
-    result.textContent = "You are Tom!"
+result.textContent = "You are Tom!"
 } else if(job === "Genuis" && suit === "Three"){
-    result.textContent = "You are Andrew!"
+result.textContent = "You are Andrew!"
 }
-    
-
-
 
 };
