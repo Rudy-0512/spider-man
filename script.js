a7d3a2d8cd4181e200d3da277ebab5615e5e0a59
Rudy-0512 <rgonzalez-s@cps.edu>
2025-04-16 14:40:02 +0000

    v6

--- script.js
+++ script.js
@@ -4,9 +4,11 @@ let Q1= document.querySelector(".job");
 let Q2= document.querySelector(".suit");
 let button= document.querySelector("button");
 let normalSpider= document.getElementById("Normal-spider");
-let spiderSuit= document.getElementById("Spider-suit");
+let spiderSuit= document.getElementById("spider-suit");
 let result=document.querySelector(".result");
-let delete = document.querySelector("delete");
+let h1 = document.querySelector("h1");
+let h3 = document.querySelector("h3");
+let Tobey = document.getElementById("tobey");
 
 
 
@@ -17,15 +19,20 @@ console.log(job);
 let suit = Q2.value;
 console.log(suit);
 
+
 if(job === "Photographer" && suit === "One") {
 console.log("Tobey")
 h1.innerHTML = "You are Tobey!";
-normalSpider.src = "pick.jpg";
-spiderSuit.style.display = "block";
+normalSpider.src = "Tobey.webp";
+spiderSuit.display = "block";
+h3.style.display = "block";
+
 } else if(job === "Engineer" && suit === "Two") {
-result.textContent = "You are Tom!"
-} else if(job === "Genuis" && suit === "Three"){
-result.textContent = "You are Andrew!"
+h1.innerHTML = "You are Tom!"
+normalSpider.src = "R.png";
+} else if(job === "Genius" && suit === "Three"){
+h1.innerHTML = "You are Andrew!"
+normalSpider.src = "drew.png";
 }
 
 };
